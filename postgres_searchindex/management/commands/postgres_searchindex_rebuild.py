5a7a6612aee62f91c426ef5464c8c974ddb4af76
Ben Stähli <bnzk@bnzk.ch>
2024-05-08 10:16:51 +0200

    fix lint

--- postgres_searchindex/management/commands/postgres_searchindex_rebuild.py
+++ postgres_searchindex/management/commands/postgres_searchindex_rebuild.py
@@ -1,27 +1,29 @@
 # commands/reindex_indexes.py
 from django.core.management import BaseCommand
+
 from ._utils import delete_indexes, update_indexes
+
 
 class Command(BaseCommand):
     help = "Reindexing postgres "
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--force',
-            action='store_true',
-            help='Force reindex without confirmation'
+            "--force", action="store_true", help="Force reindex without confirmation"
         )
 
     def handle(self, *args, **options):
-        if not options['force']:
-            self.stdout.write(self.style.WARNING(
-                "This will delete all indexes and reindex them."
-                " Are you sure you want to proceed?"
-            ))
+        if not options["force"]:
+            self.stdout.write(
+                self.style.WARNING(
+                    "This will delete all indexes and reindex them."
+                    " Are you sure you want to proceed?"
+                )
+            )
 
             confirmation = input("Type 'yes' to continue: ")
 
-            if confirmation.lower() != 'yes':
+            if confirmation.lower() != "yes":
                 self.stdout.write(self.style.ERROR("Reindexing aborted."))
                 return
 

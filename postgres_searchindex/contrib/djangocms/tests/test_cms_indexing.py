f1dc2630762729a4f57bbd295f8a0d2a60471c7f
Ben Stähli <bnzk@bnzk.ch>
2026-03-30 23:25:38 +0200

    cms4 only!

--- postgres_searchindex/contrib/djangocms/tests/test_cms_indexing.py
+++ postgres_searchindex/contrib/djangocms/tests/test_cms_indexing.py
@@ -7,7 +7,6 @@ from django.test import Client
 from django.test.testcases import TestCase
 from djangocms_versioning.models import Version
 
-from postgres_searchindex.contrib.djangocms.compat import LT_CMS_40
 from postgres_searchindex.models import IndexEntry
 from postgres_searchindex.tests.test_app.cms_plugins import TestPlugin
 
@@ -25,28 +24,22 @@ class CMSIndexingTests(TestCase):
 
         page = create_page("page_en", "base.html", "en", created_by=user)
         create_title("de", "page_de", page, created_by=user)
-        if LT_CMS_40:
-            placeholder_en = page.get_placeholders(slot="content")
-        else:
-            page_content_en = PageContent.admin_manager.filter(
-                page=page, language="en"
-            ).first()
-            rescan_placeholders_for_obj(page_content_en)
-            placeholder_en = page_content_en.placeholders.get(slot="content")
+        page_content_en = PageContent.admin_manager.filter(
+            page=page, language="en"
+        ).first()
+        rescan_placeholders_for_obj(page_content_en)
+        placeholder_en = page_content_en.placeholders.get(slot="content")
         add_plugin(placeholder_en, TestPlugin, "en", field1=the_content)
 
         call_command("postgres_searchindex_update")
         qs = IndexEntry.objects.all()
+        # none yet!
         self.assertEqual(qs.count(), 0)
-        # English page should have the text plugin
-        if LT_CMS_40:
-            page.publish("en")
-        else:
-            Version.objects.get_for_content(page_content_en).publish(user)
+        # publish, check content
+        Version.objects.get_for_content(page_content_en).publish(user)
         content_en = self.client.get(page.get_absolute_url())
         self.assertRegex(str(content_en.content), the_content)
-
-        # index
+        # check index
         call_command("postgres_searchindex_update")
         qs = IndexEntry.objects.all()
         self.assertEqual(qs.filter(content__contains=the_content).count(), 1)
